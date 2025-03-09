0cccbfb4a319520bd4626a9bf07d9eff4939ee43
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2025-03-09 15:23:32 +0100

    Change deprecated methods

--- src/main/kotlin/fr/ftnl/locked/twitch/listeners/AppRaidEvent.kt
+++ src/main/kotlin/fr/ftnl/locked/twitch/listeners/AppRaidEvent.kt
@@ -3,6 +3,7 @@ package fr.ftnl.locked.twitch.listeners
 import com.github.philippheuer.events4j.simple.domain.EventSubscriber
 import com.github.twitch4j.chat.events.channel.RaidEvent
 import fr.ftnl.locked.Locked
+import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.entity.EntityType
@@ -19,7 +20,7 @@ class AppRaidEvent(val locked: Locked) {
             locked.server.onlinePlayers.filter { it.gameMode == GameMode.SURVIVAL }.forEach {
                 for (ignored in 1..min(event.viewers, locked.config.twitchConfig.chickenRaid.maxRaidChickens)) {
                     val e = it.world.spawnEntity(it.location, EntityType.CHICKEN)
-                    e.customName = "§6Raid Chicken de§r §4§l${event.raider.name}§r"
+                    e.customName(Component.text("§6Raid Chicken de§r §4§l${event.raider.name}§r"))
                 }
             }
             
