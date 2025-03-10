b2047bf46d3995e144d111a78726c2b24f669813
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2025-03-10 14:31:01 +0100

    typo

--- src/main/kotlin/fr/ftnl/locked/config/configElements/twitch/bits/ItemSound.kt
+++ src/main/kotlin/fr/ftnl/locked/config/configElements/twitch/bits/ItemSound.kt
@@ -2,14 +2,14 @@ package fr.ftnl.locked.config.configElements.twitch.bits
 
 data class ItemSound(
     val enableEggSounds: Boolean = false,
-    val eggSoundsCoast: Int = 384,
+    val eggSoundsCost: Int = 384,
     
     val enableRandomSound: Boolean = true,
-    val randomSoundCoast: Int = 404,
+    val randomSoundCost: Int = 404,
     
     val enableTntActivationSound: Boolean = true,
-    val tntActivationSoundCoast: Int = 46,
+    val tntActivationSoundCost: Int = 46,
     
     val enableExplosionSound: Boolean = true,
-    val explosionSoundCoast: Int = 407,
+    val explosionSoundCost: Int = 407,
 )
