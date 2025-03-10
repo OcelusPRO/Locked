312dc70e658c9ec5273636be39f9b3d7872d83a4
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2025-03-10 14:29:41 +0100

    remove old code

--- src/main/kotlin/fr/ftnl/locked/twitch/listeners/AppFollowEvent.kt
+++ src/main/kotlin/fr/ftnl/locked/twitch/listeners/AppFollowEvent.kt
@@ -1,37 +1,23 @@
 package fr.ftnl.locked.twitch.listeners
 
 import com.github.philippheuer.events4j.simple.domain.EventSubscriber
-import com.github.twitch4j.chat.events.channel.FollowEvent
-import com.github.twitch4j.events.ChannelFollowCountUpdateEvent
+import com.github.twitch4j.eventsub.events.ChannelFollowEvent
 import fr.ftnl.locked.Locked
-import java.util.concurrent.atomic.AtomicInteger
 
 
 class AppFollowEvent(val locked: Locked) {
     
     val followList = mutableListOf<String>()
-    val followCounter = AtomicInteger()
     
+
     @EventSubscriber
-    fun onFollow(event: FollowEvent) {
-        println(event.user.name + " followed the channel " + event.channel.name)
-        
-        if (event.user.id in followList) return
-        followList.add(event.user.id)
+    fun onEventSubFollow(event: ChannelFollowEvent) {
+        println("New follow received with EventSub: " + event.userName)
+        if (event.userId in followList) return
+        followList.add(event.userId)
         locked.borderSizeManager.increaseBorder(
             locked.config.twitchConfig.followBorderConfig.followBorderChanger, locked.pData.currentWorldName
         )
-    }
-    
-    @EventSubscriber
-    fun onFollowCountChange(event: ChannelFollowCountUpdateEvent) {
-        println("Follow count changed: " + event.followCount)
-        if (event.followCount > followCounter.get()) {
-            locked.borderSizeManager.increaseBorder(
-                locked.config.twitchConfig.followBorderConfig.followBorderChanger, locked.pData.currentWorldName
-            )
-        }
-        followCounter.set(event.followCount)
     }
     
     
