019b61be9fef41c402df6d503a20a522edf68074
ocelusPRO <32218885+OcelusPRO@users.noreply.github.com>
2025-03-10 09:35:38 +0100

    Improve reset command

--- src/main/kotlin/fr/ftnl/locked/server/commands/NewWorldCommand.kt
+++ src/main/kotlin/fr/ftnl/locked/server/commands/NewWorldCommand.kt
@@ -2,6 +2,8 @@ package fr.ftnl.locked.server.commands
 
 import fr.ftnl.locked.Locked
 import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.World
 import org.bukkit.WorldCreator
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
@@ -20,36 +22,67 @@ class NewWorldCommand(val main: Locked) : CommandExecutor {
         
         // send message to sender
         sender.sendMessage("Création du monde $worldName") // Création d'un nouveau monde
-        val worldCreator = WorldCreator(worldName)
-        val newWorld = main.server.createWorld(worldCreator)
-            ?: return false
+        val newWorld = createWorld(worldName) ?: return false.also { sender.sendMessage("Erreur lors de la création du monde") }
+        val l = generateNewLocation(newWorld)
         
-        
-        val spl = newWorld.spawnLocation
-        
-        main.borderSizeManager.setBorderLocation(spl, newWorld.name)
+        main.borderSizeManager.setBorderLocation(l, newWorld.name)
         main.borderSizeManager.resetBorder(newWorld.name)
         
-        main.commandSender.executeCommandInWorld(newWorld.name, "spawnpoint @a ${spl.x.roundToInt()} ${spl.y.roundToInt()} ${spl.z.roundToInt()}")
+        main.commandSender.executeCommandInWorld(newWorld.name, "spawnpoint @a ${l.x.roundToInt()} ${l.y.roundToInt()} ${l.z.roundToInt()}")
         main.commandSender.executeCommand("advancement revoke @a everything")
         main.commandSender.executeCommandInWorld(newWorld.name, "clear @a")
         main.commandSender.executeCommandInWorld(newWorld.name, "experience set @a 0 points")
         main.commandSender.executeCommandInWorld(newWorld.name, "experience set @a 0 levels")
-        for (p in Bukkit.getServer().onlinePlayers) {
-            p.health = 20.0
-        }
-        for (p in Bukkit.getServer().onlinePlayers) {
-            p.foodLevel = 20
-        }
-        for (p in Bukkit.getServer().onlinePlayers) {
-            p.saturation = 10f
-        }
-        main.server.onlinePlayers.forEach { it.teleport(spl) }
         
+        main.server.onlinePlayers.forEach { p ->
+            p.health = 20.0
+            p.foodLevel = 20
+            p.saturation = 10f
+            p.teleport(l)
+        }
+        
+        removeOldWorld()
+        
+        main.pData.reset(main.config)
         main.pData.currentWorldName = newWorld.name
         main.pData.save(File(main.dataFolder, "data.json"))
         main.scoreboardManager.updateScoreboard()
         
         return true
+    }
+
+    fun createWorld(worldName: String): World? {
+        val worldCreator = WorldCreator(worldName)
+        return main.server.createWorld(worldCreator)
+    }
+    
+    fun removeOldWorld(){
+        val worldName = main.pData.currentWorldName
+        val world = Bukkit.getWorld(worldName) ?: return
+        main.server.unloadWorld(world, false)
+        val worldFolder = File(main.server.worldContainer, worldName)
+        if (worldFolder.exists()) {
+            worldFolder.deleteRecursively()
+            main.commandSender.executeCommand("say Le monde $worldName a été supprimé")
+        }
+    }
+    
+    fun generateNewLocation(world: World): Location{
+        val spl = world.spawnLocation
+        val illegalRange = -1000..1000
+        val range = -4000..4000
+        
+        var x = range.random()
+        while (x in illegalRange) { x = range.random() }
+        var z = range.random()
+        while (z in illegalRange) { z = range.random() }
+        
+        var newSpl = Location(spl.world, spl.x + x, spl.y, spl.z  + z)
+        val top = world.getHighestBlockAt(newSpl)
+        newSpl = top.location.add(0.0, 1.0, 0.0)
+        
+        for (i in 1..5) if (newSpl.clone().add(0.0, i.toDouble(), 0.0).block.isEmpty.not()) return generateNewLocation(world)
+        
+        return  newSpl
     }
 }
